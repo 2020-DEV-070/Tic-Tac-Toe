5c0cf18cc489e41d67556212cc211313444a0247
2020-DEV-070 <2020dev070@gmail.com>
2020-06-10 08:17:37 +0530

    Refactored: Extracted constants in gameboard test class for reusability

--- src/test/java/com/bnpp/kata/game/GameBoardTest.java
+++ src/test/java/com/bnpp/kata/game/GameBoardTest.java
@@ -9,6 +9,8 @@ public class GameBoardTest {
 
 	private static final char PLAYER_X = 'X';
 	private static final int ZERO = 0;
+	private static final int ONE = 1;
+	private static final char PLAYER_O = 'O';
 
 	@Test
 	public void playerShouldBeAbleToAddXInAnyPositionOfBoard() {
@@ -27,8 +29,8 @@ public class GameBoardTest {
 
 		assertThat(board.getCurrentPlayer(), CoreMatchers.is(PLAYER_X));
 
-		board.addPlayerToBoard(1, 1);
+		board.addPlayerToBoard(ONE, ONE);
 
-		assertThat(board.getCurrentPlayer(), CoreMatchers.is('O'));
+		assertThat(board.getCurrentPlayer(), CoreMatchers.is(PLAYER_O));
 	}
 }
