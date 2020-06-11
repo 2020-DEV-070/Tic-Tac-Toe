a0227bb15c5ce5b91196027219b1b07a706358d9
2020-DEV-070 <2020dev070@gmail.com>
2020-06-11 09:11:32 +0530

    Refactored: Extracted instance initialization, constant for invalid inputs in test class

--- src/test/java/com/bnpp/kata/game/TicTacToeGameRunnerTest.java
+++ src/test/java/com/bnpp/kata/game/TicTacToeGameRunnerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import java.io.ByteArrayInputStream;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bnpp.kata.game.exception.InvalidArgumentsException;
@@ -13,6 +14,7 @@ import com.bnpp.kata.game.exception.PositionOutOfValidRangeException;
 
 public class TicTacToeGameRunnerTest {
 
+	private static final String[] USER_INVALID_INPUT_EMPTY_ROW = { " ", "2" };
 	private static final String USER_INPUT_ONE = "1,2";
 	private static final String USER_INPUT_TWO = "1,1";
 	private static final String USER_INPUT_THREE = "0,2";
@@ -20,6 +22,13 @@ public class TicTacToeGameRunnerTest {
 	private static final String USER_INPUT_FIVE = "2,2";
 	private static final String WINNER_RESULT = "X is the Winner!";
 	private static final String NEW_LINE = System.getProperty("line.separator");
+
+	TicTacToeGameRunner gameRunner;
+
+	@Before
+	public void initialize() {
+		gameRunner = new TicTacToeGameRunner();
+	}
 
 	@Test
 	public void shouldReturnTheGameResultBasedOnUserInputsWhenPlayGameMethodIsCalled()
@@ -29,17 +38,16 @@ public class TicTacToeGameRunnerTest {
 				.append(USER_INPUT_FIVE);
 		System.setIn(new ByteArrayInputStream(inputStreamBuilder.toString().getBytes()));
 
-		TicTacToeGameRunner gameRunner = new TicTacToeGameRunner();
 		String gameResult = gameRunner.playGame();
+
 		assertThat(gameResult, CoreMatchers.is(WINNER_RESULT));
 	}
 
 	@Test(expected = InvalidArgumentsException.class)
 	public void shouldThrowInvalidArgumentsExceptionForInvalidInputsWhenEmptyRowIsPassedAsUserInput()
 			throws InvalidArgumentsException {
-		String[] invalidInputWithoutRow = { " ", "2" };
+		String[] invalidInputWithoutRow = USER_INVALID_INPUT_EMPTY_ROW;
 
-		TicTacToeGameRunner gameRunner = new TicTacToeGameRunner();
 		gameRunner.validateUserInputs(invalidInputWithoutRow);
 	}
 }
