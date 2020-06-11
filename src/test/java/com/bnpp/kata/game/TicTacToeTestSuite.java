24b17a43ce8c26db90674b6196215f3b20e787e0
2020-DEV-070 <2020dev070@gmail.com>
2020-06-11 11:25:19 +0530

    Added TicTacToeGameRunnerTest class in the test suite

--- src/test/java/com/bnpp/kata/game/TicTacToeTestSuite.java
+++ src/test/java/com/bnpp/kata/game/TicTacToeTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GameBoardTest.class, TicTacToeGameTest.class })
+@SuiteClasses({ GameBoardTest.class, TicTacToeGameTest.class, TicTacToeGameRunnerTest.class })
 public class TicTacToeTestSuite {
 
 }
