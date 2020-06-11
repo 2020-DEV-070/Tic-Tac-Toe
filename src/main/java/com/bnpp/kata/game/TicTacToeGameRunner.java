8d58a8d0646907ef3981e8509030993499f34f93
2020-DEV-070 <2020dev070@gmail.com>
2020-06-11 08:54:58 +0530

    Refactored: Extracted constants in main class for better readability

--- src/main/java/com/bnpp/kata/game/TicTacToeGameRunner.java
+++ src/main/java/com/bnpp/kata/game/TicTacToeGameRunner.java
@@ -7,16 +7,20 @@ import com.bnpp.kata.game.exception.PositionOutOfValidRangeException;
 import com.bnpp.kata.game.model.Position;
 
 public class TicTacToeGameRunner {
+	private static final int COLUMN_INPUT = 1;
+	private static final int ROW_INPUT = 0;
+	private static final String GAME_CONTINUES = "Game Continues";
+
 	public String playGame() throws PositionAlreadyOccupiedException, PositionOutOfValidRangeException {
 		TicTacToeGame game = new TicTacToeGame();
 		String result;
 		Scanner scan = new Scanner(System.in);
 		do {
 			String[] input = scan.nextLine().split(",");
-			result = game.play(new Position(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
+			result = game.play(new Position(Integer.parseInt(input[ROW_INPUT]), Integer.parseInt(input[COLUMN_INPUT])));
 			game.gameBoard.printBoard();
 
-		} while ((result.contains("Game Continues") && scan.hasNext()));
+		} while ((result.contains(GAME_CONTINUES) && scan.hasNext()));
 		scan.close();
 		return result;
 	}
